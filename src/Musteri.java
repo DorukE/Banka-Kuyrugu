6dd00b59a03912610fdf51cb86bcb1d96d590098
DorukE <dorukevcimik@gmail.com>
2026-01-06 10:17:37 +0300

    Update Musteri.java

--- src/Musteri.java
+++ src/Musteri.java
@@ -1,12 +1,10 @@
 /**
  * Doruk Evcimik - 240053028
  * Müşteri Veri Modeli
- * Not: Arayüz kodundaki 'numara' çağırma hatasını düzeltmek için
- * değişken adı 'numara' olarak güncellenmiştir.
  */
 public class Musteri {
     public String ad;
-    public int numara; // Arayüzle uyum için 'siraNo' yerine 'numara' yapıldı
+    public int numara;
 
     public Musteri(String ad, int numara) {
         this.ad = ad;
